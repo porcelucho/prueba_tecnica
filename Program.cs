18fe67b8127ec3ee36070aae5ca13a328f937905
Jorge Luis Higuita Isaza <jhiguita62@argos.com.co>
2023-02-09 18:20:50 -0500

    your commit message

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿namespace Prueba
+{
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("Ingrese el agente que desea ejecutar: ");
+            Console.WriteLine("[1] - Agente A");
+            Console.WriteLine("[2] - Agente B");
+            Console.WriteLine("[3] - Agente C");
+            Console.WriteLine("Ingrese la opción:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("");
+            Console.WriteLine("Ingrese el tamaño de N: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n < 0 || n > 100)
+            {
+                Console.WriteLine("Debe ingresar un tamaño entre 0 y 100.");
+            }
+            else
+            {
+                switch (num)
+                {
+                    case 1:
+                        getStairCase_AgenteA(n);
+                        break;
+                    case 2:
+                        getStairCase_AgenteB(n);
+                        break;
+                    case 3:
+                        getStairCase_AgenteC(n);
+                        break;
+                    default:
+                        Console.WriteLine("Seleccione una opción válida.");
+                        break;
+                }
+            }
+        }
+
+        static void getStairCase_AgenteA(int n)
+        {
+            for (int y = n - 1; y >= 0; y--)
+            {
+                for (int x = 0; x < n; x++)
+                {
+                    if (x >= y)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        static void getStairCase_AgenteB(int n)
+        {
+            for (int y = 0; y < n; y++)
+            {
+                for (int x = 0; x < n; x++)
+                {
+                    if (y > x)
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write("#");
+                    }
+
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        static void getStairCase_AgenteC(int n)
+        {
+
+        }
+    }
+}
---
+++ Program_Formula.cs
@@ -0,0 +1,86 @@
+﻿namespace Prueba
+{
+    class Program_Formula
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el agente que desea ejecutar: ");
+            Console.WriteLine("[1] - Agente A");
+            Console.WriteLine("[2] - Agente B");
+            Console.WriteLine("[3] - Agente C");
+            Console.WriteLine("Ingrese la opción:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("");
+            Console.WriteLine("Ingrese la lista de números: ");
+            var n = Console.ReadLine();
+            if (n != null)
+            {
+                switch (num)
+                {
+                    case 1:
+                        getMedia_AgenteA(n);
+                        break;
+                    case 2:
+                        getMediaArmonica_AgenteB(n);
+                        break;
+                    case 3:
+                        getMediana_AgenteC(n);
+                        break;
+                    default:
+                        Console.WriteLine("Seleccione una opción válida.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Debe ingresar una lista de números.");
+            }
+        }
+
+        static void getMedia_AgenteA(string n)
+        {
+            double suma = 0.0;
+            for (int y = 0; y < n.Length; y++)
+            {
+                suma = suma + Convert.ToInt32(n.Substring(y, 1));
+            }
+            double result = suma / n.Length;
+            Console.WriteLine("El resultado de la media es: " + result);
+        }
+
+        static void getMediaArmonica_AgenteB(string n)
+        {
+            double suma = 0.0;
+            for (int y = 0; y < n.Length; y++)
+            {
+                suma = suma + (1 / Convert.ToInt32(n.Substring(y, 1)));
+            }
+            double result = n.Length / suma;
+            Console.WriteLine("El resultado de la media es: " + result);
+        }
+
+        static void getMediana_AgenteC(string n)
+        {
+            int[] vec = new int[n.Length];
+            for (int y = 0; y < n.Length; y++)
+            {
+                vec[y] = Convert.ToInt32(n.Substring(y, 1));
+            }
+            Array.Sort(vec);
+            double suma = 0.0;
+            if (vec.Length % 2 == 0)
+            {
+                int mitad1 = vec[(vec.Length / 2)];
+                int mitad2 = vec[(vec.Length / 2) + 1];
+                suma = (mitad1 + mitad2) / 2;
+            }
+            else
+            {
+                decimal mitad = (vec.Length / 2) + 1;
+                int pos = Convert.ToInt32(Math.Floor(mitad));
+                suma = vec[pos - 1];
+            }
+            Console.WriteLine("El resultado de la mediana es: " + suma);
+        }
+    }
+}
